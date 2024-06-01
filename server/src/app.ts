7de4bd6cbac0485580fdb8978ae851934b43e451
Shreshth Verma <verma2007s@gmail.com>
2024-06-01 13:02:58 +0530

    feat: added socket.io

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,33 @@ import express from "express";
 import "dotenv/config";
 import pinoHttp from "pino-http";
 import logger from "./config/logger";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (data) => {
+    console.log('Message received: ', data);
+    socket.emit('message', `Echo: ${data}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.use(express.json());
 app.use(pinoHttp({ logger, useLevel: "trace" }));
 
-export default app;
+let switchIsOn = false
+
+app.get("/switch/state", (req, res) => {
+  res.status(200).send(switchIsOn)
+})
+
+export default server;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
-import app from "./app";
+import { createServer } from "http";
+import server from "./app";
 import logger from "./config/logger";
 
 logger.info("/////////////////////////////////////////////");
@@ -9,8 +10,14 @@ logger.info("//                                         //");
 logger.info("/////////////////////////////////////////////");
 logger.info("/////////////////////////////////////////////");
 
-const port = process.env.PORT ?? 3000;
+const port = Number(process.env.PORT ?? 3000);
 
-const server = app.listen(port, () => {
-  logger.info(`Server listening at http://localhost:${port}`);
-});
+server.listen(port,
+  process.env.ENVIRONMENT === 'dev' ?
+    process.env.LOCAL_IP : 'localhost',
+  () => {
+    logger.info(
+      `Server listening at http://${process.env.ENVIRONMENT === 'dev'
+        ? process.env.LOCAL_IP : 'localhost'
+      }:${port}`);
+  });

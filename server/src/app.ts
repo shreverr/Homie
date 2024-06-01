6819d1d3660172b6d57d279579134da034508668
Shreshth Verma <verma2007s@gmail.com>
2024-06-01 15:42:05 +0530

    feat: added socket events for updating switch state

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,31 +4,61 @@ import pinoHttp from "pino-http";
 import logger from "./config/logger";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import EventEmitter from "events";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+const eventEmmiter = new EventEmitter();
+
+let switchIsOn = false
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  socket.on('message', (data) => {
-    console.log('Message received: ', data);
-    socket.emit('message', `Echo: ${data}`);
+  socket.on('logs', (data) => {
+    logger.info(`ESP8266: ${data}`)
   });
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
+  eventEmmiter.on('SwitchStateChange', () => {
+    logger.info(`Sending new state to esp8266`)
+    socket.emit('SwitchStateChange', switchIsOn)
+  })
 });
 
 app.use(express.json());
 app.use(pinoHttp({ logger, useLevel: "trace" }));
 
-let switchIsOn = false
-
 app.get("/switch/state", (req, res) => {
-  res.status(200).send(switchIsOn)
+  try {
+    logger.info("GET switch state")
+    res.status(200).json({
+      state: switchIsOn,
+      status: 'success'
+    })
+  } catch (err) {
+    logger.error(err)
+
+  }
+})
+
+app.post("/switch/state", (req, res) => {
+  try {
+    const newState = req.body.state
+    logger.info(`POST switch state: ${newState}`)
+    switchIsOn = newState
+    eventEmmiter.emit('SwitchStateChange')
+    res.status(200).json({
+      state: switchIsOn,
+      status: 'success'
+    })
+  } catch (err) {
+    logger.error(err)
+  }
 })
 
 export default server;

bc5a09a0626eeaa00fe1d599f9605751b83abf4a
Shreshth Verma <verma2007s@gmail.com>
2024-08-05 21:00:22 +0530

    added multiple aps

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -15,17 +15,30 @@ SocketIOclient socketIO;
 #define RANDOM_PIN 4
 #define SWITCH_PIN 15
 
-const char* WIFI_SSID = "Gunnukanet";
-const char* WIFI_PASSWORD = "Gunnu@123";
+const char* WIFI_SSID = "lIllIllI";
+const char* WIFI_PASSWORD = "12345677";
 // const char* SERVER_IP = "homie-server.onrender.com";
 // const int PORT = 443;
 const char* SERVER_IP = "homie-server.onrender.com";
 const int PORT = 443;
 
+bool relayState = false;
+bool switchState = false;
+void switchOverRide(){
+  if(digitalRead(SWITCH_PIN) != switchState){
+    changeRelayState(!relayState);
+    switchState = digitalRead(SWITCH_PIN);
+  }
+}
+
 void socketIOEvent(socketIOmessageType_t type, uint8_t* payload, size_t length) {
   switch (type) {
     case sIOtype_DISCONNECT:
       USER_SERIAL.printf("[IOc] Disconnected!\n");
+      digitalWrite(D4, HIGH);
+      delay(50);
+      digitalWrite(D4, LOW);
+      delay(50);
       break;
     case sIOtype_CONNECT:
       USER_SERIAL.printf("[IOc] Connected to url: %s\n", payload);
@@ -97,9 +110,11 @@ void blinkLED(uint timeDelay) {
 void changeRelayState(bool state) {
   if (state) {
     USER_SERIAL.printf("changing to true\n");
+    relayState = true;
     digitalWrite(RELAY_PIN, HIGH);
   } else {
     USER_SERIAL.printf("changing to false\n");
+    relayState = false;
     digitalWrite(RELAY_PIN, LOW);
   }
 }
@@ -112,6 +127,8 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(RANDOM_PIN, OUTPUT);  // sets the digital pin 13 as output
   pinMode(SWITCH_PIN, INPUT);
+  //reading state of the switch
+  switchState = digitalRead(SWITCH_PIN);
   // Connect to wifi
   connectWifi(WIFI_SSID, WIFI_PASSWORD);
   //socket conn
@@ -125,8 +142,13 @@ int val = 0;
 void loop() {
   // blinkLED(1000);
   val = digitalRead(SWITCH_PIN);  
-  USER_SERIAL.printf("val\n %d", val);
+  // USER_SERIAL.printf("val\n %d", val);
   digitalWrite(RANDOM_PIN, val);
   reconnectToWifiIfDisconnected(WIFI_SSID, WIFI_PASSWORD);
+  switchOverRide();
+  reconnectToWifiIfDisconnected("POCO M6 Pro 5G", "connect6");
+  switchOverRide();
+  reconnectToWifiIfDisconnected("pookie", "");
+  switchOverRide();
   socketIO.loop();
 }
--- firmware/wifi.cpp
+++ firmware/wifi.cpp
@@ -1,8 +1,9 @@
 #include <ESP8266WiFi.h>
 #include "wifi.h"
 
+
+
 void connectWifi(const char* ssid,const char* password) {
-  delay(10);
 
   // Start WiFi connection
   Serial.println();
@@ -10,10 +11,16 @@ void connectWifi(const char* ssid,const char* password) {
 
   WiFi.begin(ssid, password);
 
+  int tries =0;
   // Wait for the connection to establish
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
+    if(tries>10)return;
+    tries++;
+    digitalWrite(D4, HIGH);
+    delay(250);
+    digitalWrite(D4, LOW);
+    delay(250);
     Serial.print(".");
   }
 
